500d6a7fd064e896050517f2d02e726149610c84
Mahdhi S <mahdhi.dev@gmail.com>
2025-06-26 21:08:43 +0530

    add Course entity with basic fields

--- src/main/java/com/example/lmsproject/entity/Course.java
+++ src/main/java/com/example/lmsproject/entity/Course.java
@@ -1,5 +1,22 @@
 package com.example.lmsproject.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "courses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String courseName;
 
 }
