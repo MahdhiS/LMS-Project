69ea430973bbe850db44607bb5f0739c0c1a3ace
Mahdhi S <mahdhi.dev@gmail.com>
2025-07-09 23:13:06 +0530

    Added URL link for swagger

--- src/main/java/com/example/lmsproject/config/SwaggerConfig.java
+++ src/main/java/com/example/lmsproject/config/SwaggerConfig.java
@@ -7,6 +7,9 @@ import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+
+// http://localhost:8080/swagger-ui/index.html
 @Configuration
 public class SwaggerConfig {
 
